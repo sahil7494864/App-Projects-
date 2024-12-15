acd8f81e4d14a1b3bcb52a747c9a5296bf3e3e4a
sahil <sahil895066@gmail.com>
2024-12-15 22:24:56 +0530

    message

---
+++ pages/tamplates/T9Resume.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function T9Resume() {
+  return (
+    <View>
+      <Text>T9Resume</Text>
+    </View>
+  )
+}
